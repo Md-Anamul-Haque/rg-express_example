1688d0e1912bce44d4b2c88c1036a226cd5da08a
Anamul <anamul@Anamuls-MacBook-Pro.local>
2025-05-06 15:22:23 +0600

    update rg-express

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import rg from 'rg-express';
+import { routes } from 'rg-express';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(rg.routes({
+
+app.use(routes({
   baseDir: __dirname,
-  routeGenIfEmpty: true
+  routeGenIfEmpty: true,
 }));
 
 // Start the server
